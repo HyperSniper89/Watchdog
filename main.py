cf5c7f2b5100902c070968fd4b55095b060fd18e
Hypersniper89 <2020.183@student.setur.fo>
2022-12-29 23:54:42 +0000

    watchdog code

---
+++ main.py
@@ -0,0 +1,96 @@
+#Modified by smartbuilds.io
+#Date: 27.09.20
+#Desc: This web application serves a motion JPEG stream
+# main.py
+# import the necessary packages
+from flask import Flask, render_template, Response, request, send_from_directory, jsonify
+from camera import VideoCamera
+
+import os
+import serial
+import time
+
+
+
+
+pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
+
+
+# App Globals (do not edit)
+app = Flask(__name__)
+
+@app.route("/control_master", methods=["POST"])
+def control_master():
+  value = request.form["value"]
+  command = value
+  # Do something with the value
+  
+  arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
+  if command == "w":
+      arduino.write(b'd')  # forward
+      time.sleep(1)
+  elif command == "a":
+      arduino.write(b'a')
+      time.sleep(1)
+  elif command == "d":
+      arduino.write(b'd')
+      time.sleep(1)
+  elif command == "s":  
+      arduino.write(b's')
+      time.sleep(1)
+  elif command == "b":
+      arduino.write(b'b')
+      time.sleep(1)
+  elif command == "1":
+      arduino.write(b'1')
+      time.sleep(1)
+  elif command == "2":
+      arduino.write(b'2')
+      time.sleep(1)
+  elif command == "3":
+      arduino.write(b'3')
+      time.sleep(1)
+      # Send a response to the client
+     
+  # Send a response to the client
+  serial.close()
+  return "You are now robo driving"
+
+@app.route('/')
+def watchdog():
+    return render_template('watchdog.html') #you can customze .html here
+
+""" @app.route('/road', methods=['POST', 'GET'])
+def control_motors():
+    with open("/var/www/watchdog/test", "w") as f:
+        f.wright("test")
+  # Get the command from the request
+    command = request.form['command']
+  
+    arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1) """
+ 
+
+
+   
+def gen(camera):
+    #get camera frame
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(pi_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Take a photo when pressing camera button
+@app.route('/picture')
+def take_picture():
+    pi_camera.take_picture()
+    return "None"
+    
+# if __name__ == 'main':
+    # app.run()
+
+
