6a2fc3afa52f0eb9f58c76b4147a703f22606683
Hypersniper89 <2020.183@student.setur.fo>
2022-12-30 22:30:39 +0000

    cleaned code and designed buttons

--- main.py
+++ main.py
@@ -2,19 +2,17 @@
 #Date: 27.09.20
 #Desc: This web application serves a motion JPEG stream
 # main.py
+
+#Modified by Jákup and Heidi 2022
+
 # import the necessary packages
-from flask import Flask, render_template, Response, request, send_from_directory, jsonify
+from flask import Flask, render_template, Response, request
 from camera import VideoCamera
 
-import os
 import serial
 import time
 
-
-
-
 pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
-
 
 # App Globals (do not edit)
 app = Flask(__name__)
@@ -27,51 +25,40 @@ def control_master():
   
   arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
   if command == "w":
-      arduino.write(b'd')  # forward
+      arduino.write(b'd')   # forward
       time.sleep(1)
   elif command == "a":
-      arduino.write(b'a')
+      arduino.write(b'a')   # left
       time.sleep(1)
   elif command == "d":
-      arduino.write(b'd')
+      arduino.write(b'd')   # rigt
       time.sleep(1)
   elif command == "s":  
-      arduino.write(b's')
+      arduino.write(b's')   # backward
       time.sleep(1)
   elif command == "b":
-      arduino.write(b'b')
+      arduino.write(b'b')   # break
       time.sleep(1)
   elif command == "1":
-      arduino.write(b'1')
+      arduino.write(b'1')   # look down
       time.sleep(1)
   elif command == "2":
-      arduino.write(b'2')
+      arduino.write(b'2')   # look forward
       time.sleep(1)
   elif command == "3":
-      arduino.write(b'3')
+      arduino.write(b'3')   # look up
       time.sleep(1)
-      # Send a response to the client
      
   # Send a response to the client
-  serial.close()
+  serial.close() 
   return "You are now robo driving"
-
+  
+  
 @app.route('/')
 def watchdog():
     return render_template('watchdog.html') #you can customze .html here
 
-""" @app.route('/road', methods=['POST', 'GET'])
-def control_motors():
-    with open("/var/www/watchdog/test", "w") as f:
-        f.wright("test")
-  # Get the command from the request
-    command = request.form['command']
-  
-    arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1) """
- 
 
-
-   
 def gen(camera):
     #get camera frame
     while True:
@@ -79,18 +66,8 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(pi_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-# Take a photo when pressing camera button
-@app.route('/picture')
-def take_picture():
-    pi_camera.take_picture()
-    return "None"
-    
-# if __name__ == 'main':
-    # app.run()
-
-
